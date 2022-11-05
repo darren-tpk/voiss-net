ee27b0e4970c0654c44a5a964723d52be1aad54a
Darren Tan <ptan@alaska.edu>
2022-11-05 11:00:23 -0800

    Tweak presets and add v_percent_lims in script

--- plot_spectrograms.py
+++ plot_spectrograms.py
@@ -12,44 +12,11 @@ import colorcet as cc
 ### Data are filtered in the same way (0.1 to 10 Hz) and the same window duration is used (2 s).
 
 # Define variables for functions
-network = ['AV','AV']  # SEED network code(s)
-station = ['PS1A','PS1A']  # SEED station code(s)
-channel = ['BHZ','BDF']  # SEED channel code(s)
-location = ['','']  # SEED location code(s)
-starttime = UTCDateTime(2022, 10, 21, 22, 30)  # start time for data pull and spectrogram plot
-endtime = starttime + 10*60  # end time for data pull and spectrogram plot
-pad = 60  # padding length [s]
-local = False  # pull data from local
-data_dir = None  # local data directory if pulling data from local
-client = 'IRIS'  # FDSN client for data pull
-filter_band = (0.1,10)  # frequency band for bandpass filter
-window_duration = 10  # spectrogram window duration [s]
-freq_lims = (0.1,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
-log = False  # logarithmic scale in spectrogram
-cmap = cc.cm.rainbow  # colormap for spectrogram
-figsize = (16,9)  # figure size for output
-export_path = None  # show figure in iPython
-
-# Load data (note that network, station, channel, location can be lists of equal length)
-stream = load_data(network, station, channel, location, starttime, endtime, pad=pad, local=local, data_dir=data_dir, client=client)
-
-# Process waveform
-stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
-
-# Plot spectrogram for every trace in input stream
-plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log, cmap=cc.cm.rainbow,
-                 v_percent_lims=(20,95), export_path=export_path)
-
-#%% EXAMPLE 2: Plot 30 minutes of seismic data from PS1A.
-### Data are filtered in the same way (0.1 to 10 Hz) as above, but a longer window duration is used (6 s).
-plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log, figsize=figsize, export_path=None)
-
-# Define variables for functions
-network = 'AV'  # SEED network code(s)
-station = 'PVV'  # SEED station code(s)
-channel = 'SHZ'  # SEED channel code(s)
-location = ''  # SEED location code(s)
-starttime = UTCDateTime(2022, 10, 22, 9, 0)  # start time for data pull and spectrogram plot
+network = ['AV','AV','AV','AV','AV','AV']  # SEED network code(s)
+station = ['PS4A','PVV','PS1A','HAG','PN7A','PV6A']  # SEED station code(s)
+channel = ['BHZ','SHZ','BHZ','SHZ','BHZ','SHZ']  # SEED channel code(s)
+location = ['','','','','','']  # SEED location code(s)
+starttime = UTCDateTime(2021, 10, 21, 0, 0)  # start time for data pull and spectrogram plot
 endtime = starttime + 60*60  # end time for data pull and spectrogram plot
 pad = 60  # padding length [s]
 local = False  # pull data from local
@@ -61,14 +28,48 @@ freq_lims = (0.1,10)  # frequency limits for output spectrogram. If `None`, the 
 log = False  # logarithmic scale in spectrogram
 cmap = cc.cm.rainbow  # colormap for spectrogram
 figsize = (32,9)  # figure size for output
-export_path = None  # show figure in iPython
+v_percent_lims = (20,97.5)  # colorbar limits
+export_path = None   # show figure in iPython
 
 # Load data (note that network, station, channel, location can be lists of equal length)
 stream = load_data(network, station, channel, location, starttime, endtime, pad=pad, local=local, data_dir=data_dir, client=client)
 
 # Process waveform
-stream = process_waveform(stream, remove_response=True, detrend=True, taper_length=pad, taper_percentage=None, filter_band=filter_band, verbose=True)
+stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
 
 # Plot spectrogram for every trace in input stream
 plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log, cmap=cc.cm.rainbow,
-                 v_percent_lims=(20,95), figsize=figsize, export_path=None)
+                 v_percent_lims=v_percent_lims, figsize=figsize, export_path=export_path)
+
+#%% EXAMPLE 2: Infrasound
+### Data are filtered in the same way (0.1 to 10 Hz) as above, but a longer window duration is used (6 s).
+
+# Define variables for functions
+network = ['AV','AV','AV','AV','AV','AV']  # SEED network code(s)
+station = ['PS4A','PVV','PS1A','HAG','PN7A','PV6A']  # SEED station code(s)
+channel = ['BDF','BDF','BDF','BDF','BDF','BDF']  # SEED channel code(s)
+location = ['','','','','','']  # SEED location code(s)
+starttime = UTCDateTime(2022, 10, 22, 1, 0)  # start time for data pull and spectrogram plot
+endtime = starttime + 60*60  # end time for data pull and spectrogram plot
+pad = 60  # padding length [s]
+local = False  # pull data from local
+data_dir = None  # local data directory if pulling data from local
+client = 'IRIS'  # FDSN client for data pull
+filter_band = (0.1,10)  # frequency band for bandpass filter
+window_duration = 10  # spectrogram window duration [s]
+freq_lims = (0.1,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
+log = False  # logarithmic scale in spectrogram
+cmap = cc.cm.rainbow  # colormap for spectrogram
+figsize = (32,9)  # figure size for output
+v_percent_lims = (20,97.5)  # colorbar limits
+export_path = None   # show figure in iPython
+
+# Load data (note that network, station, channel, location can be lists of equal length)
+stream = load_data(network, station, channel, location, starttime, endtime, pad=pad, local=local, data_dir=data_dir, client=client)
+
+# Process waveform
+stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
+
+# Plot spectrogram for every trace in input stream
+plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log, cmap=cc.cm.rainbow,
+                 v_percent_lims=v_percent_lims, figsize=figsize, export_path=export_path)
