71fd2e6dae9d67821d89b1651dc46869465ca3df
Darren Tan <ptan@alaska.edu>
2022-11-09 11:13:35 -0900

    Update gather waveforms line and add demean option
    Update

--- plot_spectrograms.py
+++ plot_spectrograms.py
@@ -1,22 +1,53 @@
 #%% PLOT SPECTROGRAM(S)
 
-# This script pulls in data from a local directory or queries data from a client, before processing the data and
-# plotting both the waveforms and their corresponding spectrograms in separate plots.
-
 # Import all dependencies
-from toolbox import load_data, process_waveform, plot_spectrogram
+from waveform_collection import gather_waveforms
+from toolbox import process_waveform, plot_spectrogram, plot_spectrogram_multi
 from obspy import UTCDateTime
-import colorcet as cc
 
-#%% EXAMPLE 1: Plot 10 minutes of seismic and acoustic data from PS4A.
-### Data are filtered in the same way (0.1 to 10 Hz) and the same window duration is used (2 s).
+#%% EXAMPLE 1: SEISMIC
+
+### Define variables for functions
+source = 'IRIS'
+network = 'AV'
+station = 'PN7A,PS1A,PS4A,PV6A,PVV'
+location = ''
+channel = '*HZ'
+starttime = UTCDateTime(2021, 8, 5, 6, 0)  # start time for data pull and spectrogram plot
+endtime = starttime + 60*60  # end time for data pull and spectrogram plot
+pad = 60  # padding length [s]
+local = False  # pull data from local
+data_dir = None  # local data directory if pulling data from local
+client = 'IRIS'  # FDSN client for data pull
+filter_band = (0.1,10)  # frequency band for bandpass filter
+window_duration = 10  # spectrogram window duration [s]
+freq_lims = (0.5,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
+log = False  # logarithmic scale in spectrogram
+demean = False  # remove the temporal mean of the plotted time span from the spectrogram matrix
+v_percent_lims = (40,97.5)  # colorbar limits
+export_path = None   # show figure in iPython
+
+# Load data using waveform_collection tool
+stream = gather_waveforms(source=source, network=network, station=station, location=location, channel=channel, starttime=starttime-pad, endtime=endtime+pad)
+
+# Process waveform
+stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
+
+## Plot spectrogram for every trace in input stream
+#plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log=log, demean=demean, v_percent_lims=v_percent_lims, export_path=export_path)
+
+# Plot all spectrograms on one figure
+plot_spectrogram_multi(stream, starttime, endtime, window_duration, freq_lims, log=log, demean=demean, v_percent_lims=v_percent_lims, export_path=export_path)
+
+#%% EXAMPLE 2: INFRASOUND
 
 # Define variables for functions
-network = ['AV','AV','AV','AV','AV','AV']  # SEED network code(s)
-station = ['PS4A','PVV','PS1A','HAG','PN7A','PV6A']  # SEED station code(s)
-channel = ['BHZ','SHZ','BHZ','SHZ','BHZ','SHZ']  # SEED channel code(s)
-location = ['','','','','','']  # SEED location code(s)
-starttime = UTCDateTime(2021, 10, 21, 0, 0)  # start time for data pull and spectrogram plot
+source = 'IRIS'
+network = 'AV'
+station = 'PN7A,PS1A,PS4A,PV6A,PVV'
+location = ''
+channel = '*DF'
+starttime = UTCDateTime(2021, 8, 7, 9, 30)  # start time for data pull and spectrogram plot
 endtime = starttime + 60*60  # end time for data pull and spectrogram plot
 pad = 60  # padding length [s]
 local = False  # pull data from local
@@ -26,50 +57,18 @@ filter_band = (0.1,10)  # frequency band for bandpass filter
 window_duration = 10  # spectrogram window duration [s]
 freq_lims = (0.1,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
 log = False  # logarithmic scale in spectrogram
-cmap = cc.cm.rainbow  # colormap for spectrogram
-figsize = (32,9)  # figure size for output
+demean = False  # remove the temporal mean of the plotted time span from the spectrogram matrix
 v_percent_lims = (20,97.5)  # colorbar limits
 export_path = None   # show figure in iPython
 
-# Load data (note that network, station, channel, location can be lists of equal length)
-stream = load_data(network, station, channel, location, starttime, endtime, pad=pad, local=local, data_dir=data_dir, client=client)
+# Load data using waveform_collection tool
+stream = gather_waveforms(source=source, network=network, station=station, location=location, channel=channel, starttime=starttime-pad, endtime=endtime+pad)
 
 # Process waveform
 stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
 
-# Plot spectrogram for every trace in input stream
-plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log, cmap=cc.cm.rainbow,
-                 v_percent_lims=v_percent_lims, figsize=figsize, export_path=export_path)
+## Plot spectrogram for every trace in input stream
+#plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log=log, demean=demean, v_percent_lims=v_percent_lims, export_path=export_path)
 
-#%% EXAMPLE 2: Infrasound
-### Data are filtered in the same way (0.1 to 10 Hz) as above, but a longer window duration is used (6 s).
-
-# Define variables for functions
-network = ['AV','AV','AV','AV','AV','AV']  # SEED network code(s)
-station = ['PS4A','PVV','PS1A','HAG','PN7A','PV6A']  # SEED station code(s)
-channel = ['BDF','BDF','BDF','BDF','BDF','BDF']  # SEED channel code(s)
-location = ['','','','','','']  # SEED location code(s)
-starttime = UTCDateTime(2022, 10, 22, 1, 0)  # start time for data pull and spectrogram plot
-endtime = starttime + 60*60  # end time for data pull and spectrogram plot
-pad = 60  # padding length [s]
-local = False  # pull data from local
-data_dir = None  # local data directory if pulling data from local
-client = 'IRIS'  # FDSN client for data pull
-filter_band = (0.1,10)  # frequency band for bandpass filter
-window_duration = 10  # spectrogram window duration [s]
-freq_lims = (0.1,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
-log = False  # logarithmic scale in spectrogram
-cmap = cc.cm.rainbow  # colormap for spectrogram
-figsize = (32,9)  # figure size for output
-v_percent_lims = (20,97.5)  # colorbar limits
-export_path = None   # show figure in iPython
-
-# Load data (note that network, station, channel, location can be lists of equal length)
-stream = load_data(network, station, channel, location, starttime, endtime, pad=pad, local=local, data_dir=data_dir, client=client)
-
-# Process waveform
-stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
-
-# Plot spectrogram for every trace in input stream
-plot_spectrogram(stream, starttime, endtime, window_duration, freq_lims, log, cmap=cc.cm.rainbow,
-                 v_percent_lims=v_percent_lims, figsize=figsize, export_path=export_path)
+# Plot all spectrograms on one figure
+plot_spectrogram_multi(stream, starttime, endtime, window_duration, freq_lims, log=log, demean=False, v_percent_lims=v_percent_lims, export_path=export_path)
--- plot_spectrograms_bulk.py
+++ plot_spectrograms_bulk.py
@@ -1,18 +1,16 @@
 #%% PLOT SPECTROGRAM(S) BULK
 
-# This script pulls in data from a local directory or queries data from a client, before processing the data and
-# plotting both the waveforms and their corresponding spectrograms in separate plots.
-
 # Import all dependencies
 import numpy as np
-from waveform_collection import gather_waveforms_bulk
-from toolbox import process_waveform, plot_spectrogram_multi
+from waveform_collection import gather_waveforms
+from toolbox import process_waveform, plot_spectrogram, plot_spectrogram_multi
 from obspy import UTCDateTime
 
 ### Define variables for functions
-volc_lat = 55.417
-volc_lon = -161.894
-search_radius = 25
+source = 'IRIS'
+network = 'AV'
+station = 'PN7A,PS1A,PS4A,PV6A,PVV'
+location = ''
 channel = '*HZ'
 starttime = UTCDateTime(2021, 8, 4, 17, 00)  # start time for data pull and spectrogram plot
 endtime = UTCDateTime(2021, 8, 6, 17, 00)  # end time for data pull and spectrogram plot
@@ -24,6 +22,7 @@ filter_band = (0.1,10)  # frequency band for bandpass filter
 window_duration = 10  # spectrogram window duration [s]
 freq_lims = (0.1,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
 log = False  # logarithmic scale in spectrogram
+demean = False  # remove the temporal mean of the plotted time span from the spectrogram matrix
 v_percent_lims = (20,97.5)  # colorbar limits
 export_path = '/Users/darrentpk/Desktop/GitHub/tremor_ml/to_label_seis/'   # show figure in iPython
 
@@ -38,10 +37,10 @@ for i in range(total_hours):
     t2 = starttime + (i+1)*3600
 
     # Load data using waveform_collection tool
-    stream = gather_waveforms_bulk(lon_0=volc_lon, lat_0=volc_lat, max_radius=search_radius, starttime=t1-pad, endtime=t2+pad, channel=channel)
+    stream = gather_waveforms(source=source, network=network, station=station, location=location, channel=channel, starttime=starttime-pad, endtime=endtime+pad)
 
     # Process waveform
     stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=None, verbose=True)
 
     # Plot all spectrograms on one figure
-    plot_spectrogram_multi(stream,t1,t2,window_duration,freq_lims,log,v_percent_lims=v_percent_lims,export_path=export_path)
+    plot_spectrogram_multi(stream, starttime, endtime, window_duration, freq_lims, log=log, demean=demean, v_percent_lims=v_percent_lims, export_path=export_path)
