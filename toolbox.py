8fc21c86230f4759ffbc215d37c9ef4750ca4115
Darren Tan <ptan@alaska.edu>
2022-11-09 11:13:58 -0900

    Add demean argument

--- toolbox.py
+++ toolbox.py
@@ -4,7 +4,9 @@ from obspy import UTCDateTime, read, Stream, Trace
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import dates
+from matplotlib.transforms import Bbox
 from scipy.signal import spectrogram
+import colorcet as cc
 
 def load_data(network,station,channel,location,starttime,endtime,pad=None,local=None,data_dir=None,client=None):
 
@@ -184,7 +186,7 @@ def process_waveform(stream,remove_response=True,detrend=True,taper_length=None,
             print('Waveform bandpass filtered from %.2f Hz to %.2f Hz.' % (filter_band[0],filter_band[1]))
     return stream
 
-def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log,v_percent_lims=(20,100),cmap='inferno',figsize=(16,9),export_path=None):
+def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log=False,demean=False,v_percent_lims=(20,100),cmap=cc.cm.rainbow,figsize=(32,9),export_path=None):
 
     """
     Plot all traces in a stream and their corresponding spectrograms in separate plots using matplotlib
@@ -195,7 +197,8 @@ def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log,v_pe
     :param freq_lims (tuple): Tuple of length 2 storing minimum frequency and maximum frequency for the spectrogram plot ([Hz],[Hz])
     :param log (bool): If `True`, plot spectrogram with logarithmic y-axis
     :param v_percent_lims (tuple): Tuple of length 2 storing the percentile values in the spectrogram matrix used as colorbar limits. Default is (20,100).
-    :param cmap (str): Colormap for spectrogram plot. Default is 'inferno'.
+    :param cmap (str or :class:`matplotlib.colors.LinearSegmentedColormap`): Colormap for spectrogram plot. Default is colorcet.cm.rainbow.
+    :param figsize (tuple): Tuple of length 2 storing output figure size. Default is (32,9).
     :param export_path (str or `None`): If str, export plotted figures as '.png' files, named by the trace id and time. If `None`, show figure in interactive python.
     """
 
@@ -205,6 +208,7 @@ def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log,v_pe
 
     # Loop over each trace in the stream
     for trace in stream:
+
         # Check if input trace is infrasound (SEED channel name ends with 'DF'). Else, assume seismic.
         if trace.stats.channel[1:] == 'DF':
             # If infrasound, define corresponding axis labels and reference values
@@ -229,6 +233,12 @@ def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log,v_pe
         # Convert spectrogram matrix to decibels for plotting
         spec_db = 10 * np.log10(abs(spec) / (REFERENCE_VALUE**2))
 
+        # If demeaning is desired, remove temporal mean from spectrogram
+        if demean:
+            # column_ht = np.shape(spec_db)[0]
+            # ratio_vec = np.linspace(0,0.5,column_ht).reshape(column_ht,1)
+            spec_db = spec_db - np.mean(spec_db,1)[:, np.newaxis]*ratio_vec
+
         # Convert trace times to matplotlib dates
         trace_time_matplotlib = trace.stats.starttime.matplotlib_date + (segment_times / dates.SEC_PER_DAY)
 
@@ -243,18 +253,18 @@ def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log,v_pe
         # Craft figure
         fig, (ax1,ax2) = plt.subplots(2,1,figsize=figsize,constrained_layout=True)
         if freq_lims is not None:
-            c = ax1.imshow(spec_db, extent=[trace_time_matplotlib[0], trace_time_matplotlib[-1],
-                                            sample_frequencies[0], sample_frequencies[-1]],
-                                           origin='lower', aspect='auto', interpolation=None, cmap=cmap)
             freq_min = freq_lims[0]
             freq_max = freq_lims[1]
             spec_db_plot = spec_db[np.where((sample_frequencies>freq_min) & (sample_frequencies<freq_max)),:]
-            c = ax1.pcolormesh(trace_time_matplotlib, sample_frequencies, spec_db, vmin=np.percentile(spec_db_plot,v_percent_lims[0]), vmax=np.percentile(spec_db,v_percent_lims[1]), cmap=cmap, shading='nearest', rasterized=True)
+            c = ax1.imshow(spec_db, extent=[trace_time_matplotlib[0], trace_time_matplotlib[-1],sample_frequencies[0], sample_frequencies[-1]],
+                           vmin=np.percentile(spec_db_plot, v_percent_lims[0]), vmax=np.percentile(spec_db_plot, v_percent_lims[1]),
+                           origin='lower', aspect='auto', interpolation=None, cmap=cmap)
+            # c = ax1.pcolormesh(trace_time_matplotlib, sample_frequencies, spec_db, vmin=np.percentile(spec_db_plot,v_percent_lims[0]), vmax=np.percentile(spec_db_plot,v_percent_lims[1]), cmap=cmap, shading='nearest', rasterized=True)
         else:
-            c = ax1.pcolormesh(trace_time_matplotlib, sample_frequencies, spec_db, vmin=np.percentile(spec_db,v_percent_lims[0]),vmax=np.percentile(spec_db,v_percent_lims[1]), cmap=cmap, shading='nearest', rasterized=True)
-        # c.set_clim(np.percentile(spec_db,v_percent_lims[0]),
-        #            np.percentile(spec_db,v_percent_lims[1]))
-        # #c.set_clim(v_percent_lims[0], v_percent_lims[1])
+            c = ax1.imshow(spec_db, extent=[trace_time_matplotlib[0], trace_time_matplotlib[-1], sample_frequencies[0],sample_frequencies[-1]],
+                           vmin=np.percentile(spec_db, v_percent_lims[0]),  vmax=np.percentile(spec_db, v_percent_lims[1]),
+                           origin='lower', aspect='auto', interpolation=None, cmap=cmap)
+            # c = ax1.pcolormesh(trace_time_matplotlib, sample_frequencies, spec_db, vmin=np.percentile(spec_db,v_percent_lims[0]),vmax=np.percentile(spec_db,v_percent_lims[1]), cmap=cmap, shading='nearest', rasterized=True)
         ax1.set_ylabel('Frequency (Hz)',fontsize=22)
         if log:
             ax1.set_yscale('log')
@@ -286,3 +296,106 @@ def plot_spectrogram(stream,starttime,endtime,window_duration,freq_lims,log,v_pe
             fig.savefig(export_path + file_label + 'spec.png',bbox_inches=extent)
             plt.close()
 
+def plot_spectrogram_multi(stream,starttime,endtime,window_duration,freq_lims,log=False,demean=False,v_percent_lims=(20,100),cmap=cc.cm.rainbow,export_path=None):
+
+    """
+    Plot all traces in a stream and their corresponding spectrograms in separate plots using matplotlib
+    :param stream (:class:`~obspy.core.stream.Stream`): Input data
+    :param starttime (:class:`~obspy.core.utcdatetime.UTCDateTime`): Start time for desired plot
+    :param endtime (:class:`~obspy.core.utcdatetime.UTCDateTime`): End time for desired plot
+    :param window_duration (float): Duration of spectrogram window [s]
+    :param freq_lims (tuple): Tuple of length 2 storing minimum frequency and maximum frequency for the spectrogram plot ([Hz],[Hz])
+    :param log (bool): If `True`, plot spectrogram with logarithmic y-axis
+    :param v_percent_lims (tuple): Tuple of length 2 storing the percentile values in the spectrogram matrix used as colorbar limits. Default is (20,100).
+    :param cmap (str or :class:`matplotlib.colors.LinearSegmentedColormap`): Colormap for spectrogram plot. Default is colorcet.cm.rainbow.
+    :param export_path (str or `None`): If str, export plotted figures as '.png' files, named by the trace id and time. If `None`, show figure in interactive python.
+    """
+
+    # Initialize figure based on length of input stream
+    figsize = (32, 4 * len(stream))
+    fig, axs = plt.subplots(len(stream), 1, figsize=figsize)
+    fig.subplots_adjust(hspace=0)
+
+    # Loop over each trace in the stream
+    for axs_index, trace in enumerate(stream):
+
+        # Check if input trace is infrasound (SEED channel name ends with 'DF'). Else, assume seismic.
+        if trace.stats.channel[1:] == 'DF':
+            # If infrasound, define corresponding reference values
+            REFERENCE_VALUE = 20 * 10 ** -6  # Pressure, in Pa
+            rescale_factor = 1  # Plot waveform in Pa
+        else:
+            # If seismic, define corresponding reference values
+            REFERENCE_VALUE = 1  # Velocity, in m/s
+            rescale_factor = 10 ** -6  # Plot waveform in micrometer/s
+
+        # Extract trace information for FFT
+        sampling_rate = trace.stats.sampling_rate
+        samples_per_segment = int(window_duration * sampling_rate)
+
+        # Compute spectrogram (Note that overlap is 90% of samples_per_segment)
+        sample_frequencies, segment_times, spec = spectrogram(trace.data, sampling_rate, window='hann',
+                                                              scaling='density', nperseg=samples_per_segment,
+                                                              noverlap=samples_per_segment * .9)
+
+        # Convert spectrogram matrix to decibels for plotting
+        spec_db = 10 * np.log10(abs(spec) / (REFERENCE_VALUE ** 2))
+
+        # If demeaning is desired, remove temporal mean from spectrogram
+        if demean:
+            # column_ht = np.shape(spec_db)[0]
+            # ratio_vec = np.linspace(0,0.5,column_ht).reshape(column_ht,1)
+            spec_db = spec_db - np.mean(spec_db,1)[:, np.newaxis]*ratio_vec
+
+        # Convert trace times to matplotlib dates
+        trace_time_matplotlib = trace.stats.starttime.matplotlib_date + (segment_times / dates.SEC_PER_DAY)
+
+        # Craft figure
+        if freq_lims is not None:
+            freq_min = freq_lims[0]
+            freq_max = freq_lims[1]
+            spec_db_plot = spec_db[np.where((sample_frequencies > freq_min) & (sample_frequencies < freq_max)), :]
+            c = axs[axs_index].imshow(spec_db,
+                                      extent=[trace_time_matplotlib[0], trace_time_matplotlib[-1],
+                                              sample_frequencies[0],
+                                              sample_frequencies[-1]],
+                                      vmin=np.percentile(spec_db_plot, v_percent_lims[0]),
+                                      vmax=np.percentile(spec_db_plot, v_percent_lims[1]),
+                                      origin='lower', aspect='auto', interpolation=None, cmap=cmap)
+            # c = ax1.pcolormesh(trace_time_matplotlib, sample_frequencies, spec_db, vmin=np.percentile(spec_db_plot,v_percent_lims[0]), vmax=np.percentile(spec_db_plot,v_percent_lims[1]), cmap=cmap, shading='nearest', rasterized=True)
+        else:
+            c = axs[axs_index].imshow(spec_db,
+                                      extent=[trace_time_matplotlib[0], trace_time_matplotlib[-1],
+                                              sample_frequencies[0],
+                                              sample_frequencies[-1]],
+                                      vmin=np.percentile(spec_db, v_percent_lims[0]),
+                                      vmax=np.percentile(spec_db, v_percent_lims[1]),
+                                      origin='lower', aspect='auto', interpolation=None, cmap=cmap)
+            # c = ax1.pcolormesh(trace_time_matplotlib, sample_frequencies, spec_db, vmin=np.percentile(spec_db,v_percent_lims[0]),vmax=np.percentile(spec_db,v_percent_lims[1]), cmap=cmap, shading='nearest', rasterized=True)
+        if log:
+            axs[axs_index].set_yscale('log')
+        if freq_lims is not None:
+            axs[axs_index].set_ylim([freq_min, freq_max])
+            axs[axs_index].set_yticks(range(2, freq_max + 1, 2))
+        axs[axs_index].set_xlim([starttime.matplotlib_date, endtime.matplotlib_date])
+        axs[axs_index].tick_params(axis='y', labelsize=18)
+        axs[axs_index].set_ylabel(trace.id, fontsize=22, fontweight='bold')
+    if ((endtime - starttime) % 1800) == 0:
+        time_tick_list = np.arange(starttime, endtime + 1, (endtime - starttime) / 12)
+    else:
+        time_tick_list = np.arange(starttime, endtime + 1, (endtime - starttime) / 10)
+    time_tick_list_mpl = [t.matplotlib_date for t in time_tick_list]
+    time_tick_labels = [time.strftime('%H:%M') for time in time_tick_list]
+    axs[-1].set_xticks(time_tick_list_mpl)
+    axs[-1].set_xticklabels(time_tick_labels, fontsize=22, rotation=30)
+    axs[-1].set_xlabel('UTC Time on ' + starttime.date.strftime('%b %d, %Y'), fontsize=25)
+    if export_path is None:
+        fig.show()
+    else:
+        file_label = starttime.strftime('%Y%m%d_%H%M') + '__' + endtime.strftime('%Y%m%d_%H%M') + '_' + '_'.join([tr.id.split('.')[1] for tr in stream])
+        fig.savefig(export_path + file_label + '.png', bbox_inches='tight')
+        extent1 = axs[0].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        extent2 = axs[-1].get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        extent = Bbox([extent2._points[0], extent1._points[1]])
+        fig.savefig(export_path + file_label + '_spec.png', bbox_inches=extent)
+        plt.close()
