9326460adebc021ec6134608c100e8879d942281
Darren Tan <ptan@alaska.edu>
2022-11-09 17:13:24 -0900

    Update debugger to play with libcomcat

--- debugger.py
+++ debugger.py
@@ -1,1 +1,20 @@
 ## Workspace for debugging
+
+from libcomcat.search import search
+from obspy import UTCDateTime
+
+t1=UTCDateTime(2021,7,1)
+t2=UTCDateTime(2021,8,30)
+earthquakes = search(starttime=t1.datetime,
+					 endtime=t2.datetime,
+					 latitude=55.417,
+    			 	 longitude=-161.894,
+    			     maxradiuskm=275,
+					 reviewstatus='reviewed')
+
+print('Found {:g} earthquakes.'.format(len(earthquakes)))
+
+for eq in earthquakes:
+	if eq.magnitude == 8.2:
+		print(eq)
+
