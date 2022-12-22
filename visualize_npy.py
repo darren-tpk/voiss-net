04141866cf635953f9a4de3e878403d19e554f0c
Darren Tan <ptan@alaska.edu>
2022-12-21 22:08:07 -0800

    remove axes if number of chosen filepaths less than N

--- visualize_npy.py
+++ visualize_npy.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import colorcet as cc
 
 # Define numpy directory
-npy_dir = '/Users/darrentpk/Desktop/labeled_npy/'
+npy_dir = '/Users/darrentpk/Desktop/PVV_npy/'
 
 # Prepare label dictionary
 label_dict = {'Broadband Tremor': 1,
@@ -29,19 +29,23 @@ for label_check in labels:
     # Filter filenames
     all_filenames = glob.glob(npy_dir + station_check + '*' + str(label_dict[label_check]) + '.npy')
 
-    # Plot 16
+    # Plot 25
     N = 16
 
-    # printing n elements from list
+    # Randomly sample N files from list
     chosen_filenames = random.sample(all_filenames, N)
 
-    fig.suptitle('%d samples of %s slices on %s' % (N,label_check,station_check), fontweight='bold')
+    # Craft title
+    fig, axs = plt.subplots(nrows=int(np.sqrt(N)), ncols=int(np.sqrt(N)), figsize=(6,8))
+    fig.suptitle('%d samples of %s slices on %s' % (N, label_check, station_check), fontweight='bold')
     for i in range(int(np.sqrt(N))):
         for j in range(int(np.sqrt(N))):
             filename_index = i * int(np.sqrt(N)) + (j + 1) - 1
+            if filename_index > (len(chosen_filenames)-1):
+                continue
             spec_db = np.load(chosen_filenames[filename_index])
-            if np.sum(spec_db<-250) > 0:
-                print(i,j)
+            if np.sum(spec_db < -250) > 0:
+                print(i, j)
             axs[i, j].imshow(spec_db, vmin=np.percentile(spec_db, 20), vmax=np.percentile(spec_db, 97.5),
                            origin='lower', aspect='auto', interpolation=None, cmap=cc.cm.rainbow)
             axs[i, j].set_xticks([])
