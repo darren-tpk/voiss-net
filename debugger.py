9db6952afaf6edf39c9014ab6357aebdb4fde3d8
Darren Tan <ptan@alaska.edu>
2023-02-24 14:40:26 -0900

    upload models

--- debugger.py
+++ debugger.py
@@ -1,65 +1,78 @@
-# Make and save big time series
-from waveform_collection import gather_waveforms
-from toolbox import compute_metrics
-from obspy import UTCDateTime
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import medfilt
+import numpy as np
+import pandas as pd
+from obspy import UTCDateTime
 
-###### load and plot stuff
-output_dir = '/Users/darrentpk/Desktop/GitHub/tremor_ml/metrics/'
+dir = '/Users/darrentpk/Desktop/aniakchakplot/'
+repeater_file = dir + 'catalog.txt'
+orphan_file = dir + 'orphancatalog.txt'
+catalog_file = dir + 'earthquakes.csv'
 
-for kernel_size in [7]:
-    tmpl = np.load(output_dir + 'tmpl_20210101_20230101.npy')
-    tmpl2 = np.load('/Users/darrentpk/Desktop/tmpl_all.npy')
-    dr = np.load('/Users/darrentpk/Desktop/dr_all.npy')
-    pe = np.load(output_dir + 'pe_20210101_20230101.npy')
-    fc = np.load(output_dir + 'fc_20210101_20230101.npy')
-    fd = np.load(output_dir + 'fd_20210101_20230101.npy')
-    fsd = np.load(output_dir + 'fsd_20210101_20230101.npy')
-    dr = medfilt(dr, kernel_size=kernel_size)
-    pe = medfilt(pe, kernel_size=kernel_size)
-    fc = medfilt(fc, kernel_size=kernel_size)
-    fd = medfilt(fd, kernel_size=kernel_size)
-    fsd = medfilt(fsd, kernel_size=kernel_size)
+rep_pd = pd.read_csv(repeater_file, names=['cluster','time'],header=None,index_col=None, delimiter=' ')
+rep_t = rep_pd['time'].tolist()
+repeaters = [UTCDateTime(t) for t in rep_t]
 
-    fig, axs = plt.subplots(5, 1, figsize=(32, 8))
-    fig.subplots_adjust(hspace=0)
-    axs[0].plot(tmpl2, dr, color='k', alpha=0.8)
-    axs[0].set_ylabel('$D_R$ (cm$^2$)', fontsize=22)
-    axs[0].set_ylim([0,5])
-    axs[0].axhline(y=np.nanmedian(dr), color='red', linewidth=2.5, linestyle='--')
-    axs[1].plot(tmpl, pe, color='k', alpha=0.85)
-    axs[1].set_ylabel('$p_e$', fontsize=24)
-    axs[1].axhline(y=np.nanmedian(pe),color='red',linewidth=2.5, linestyle='--')
-    axs[2].plot(tmpl, fc, color='k', alpha=0.8)
-    axs[2].set_ylabel('$f_c$ (Hz)', fontsize=22)
-    axs[2].set_ylim([2.25,6])
-    axs[2].axhline(y=np.nanmedian(fc),color='red',linewidth=2.5, linestyle='--')
-    axs[3].plot(tmpl, fd, color='k', alpha=0.8)
-    axs[3].set_ylabel('$f_d$ (Hz)', fontsize=22)
-    axs[3].set_ylim([0.75,4])
-    axs[3].axhline(y=np.nanmedian(fd),color='red',linewidth=2.5, linestyle='--')
-    axs[4].plot(tmpl, fsd, color='k', alpha=0.8)
-    axs[4].set_ylabel('$\sigma_f$ (Hz)', fontsize=22)
-    axs[4].set_ylim([0,5e-4])
-    axs[4].axhline(y=np.nanmedian(fsd),color='red',linewidth=2.5, linestyle='--')
-    starttime = UTCDateTime(2021,1,1)
-    endtime = UTCDateTime(2023,1,1)
-    denominator = 24
-    time_tick_list = np.arange(starttime, endtime, (endtime - starttime) / denominator)
-    time_tick_list_mpl = [t.matplotlib_date for t in time_tick_list]
-    time_tick_labels = [time.strftime('%b \'%y') for time in time_tick_list]
-    for j in range(5):
-        axs[j].set_xlim([starttime.matplotlib_date, endtime.matplotlib_date])
-        axs[j].tick_params(axis='y', labelsize=18)
-        axs[j].set_xticks(time_tick_list_mpl)
-        axs[j].set_xticklabels([])
-    axs[-1].set_xticklabels(time_tick_labels, fontsize=20, rotation=30)
-    axs[-1].set_xlabel('Time', fontsize=28)
-    axs[-1].ticklabel_format(axis='y', style='scientific', scilimits=(-5,-5))
-    axs[-1].get_yaxis().get_offset_text().set_visible(False)
-    axs[-1].annotate(r'$\times$10$^{-5}}$', xy=(.001, .75), xycoords='axes fraction', fontsize=18)
-    plt.savefig('/Users/darrentpk/Desktop/timeline9.png',bbox_inches='tight',transparent=True)
-    fig.show()
+orp_pd = pd.read_csv(orphan_file, names=['time'],header=None,index_col=None, delimiter=' ')
+orp_t = orp_pd['time'].tolist()
+orphans = [UTCDateTime(t) for t in orp_t]
+
+cat_pd = pd.read_csv(catalog_file)
+cat_t = cat_pd['Date/Time UTC'].tolist()
+catalog = [UTCDateTime(t) for t in cat_t]
+
+base_time = UTCDateTime(2022,10,1)
+end_time = UTCDateTime(2023,2,21)
+day_vec = np.arange(base_time,end_time,86400)
+
+rep_count = []
+orp_count = []
+both_count = []
+cat_count = []
+time_vec = []
+
+for day in day_vec:
+	time_vec.append((day-base_time)/86400)
+	rep_count.append(np.sum([1 for rep in repeaters if rep<day]))
+	orp_count.append(np.sum([1 for orp in orphans if orp<day]))
+	both_count.append(rep_count[-1] + orp_count[-1])
+	cat_count.append(np.sum([1 for cat in catalog if cat<day]))
+
+xtick_utc = [UTCDateTime(2022,9,1),
+			  UTCDateTime(2022,10,1),
+			  UTCDateTime(2022,11,1),
+			  UTCDateTime(2022,12,1),
+			  UTCDateTime(2023,1,1),
+			  UTCDateTime(2023,2,1)]
+xtick_days = [(t - base_time)/86400 for t in xtick_utc]
+xtick_labels = [t.strftime('%b \'%y') for t in xtick_utc]
+
+plt.figure()
+plt.step(time_vec,rep_count,'r-',label='Repeaters')
+# plt.step(time_vec,orp_count,'b-',label='Orphans')
+# plt.step(time_vec,both_count,color='purple',label='Combined')
+plt.step(time_vec,cat_count,'k-',label='AVO catalog')
+plt.legend()
+plt.title('Aniakchak REDPy vs AVO catalog events, 2022-08-15 to 2023-02-21')
+plt.ylabel('Number of events')
+plt.xlabel('UTC Date')
+plt.xticks(xtick_days,xtick_labels)
+plt.xlim([0, (UTCDateTime(2023,2,15)-base_time)/86400])
+plt.grid()
+plt.show()
+
+clusters = rep_pd['cluster'].tolist()
+unique_clusters = list(set(clusters))
+clust_count = []
+for unique_cluster in unique_clusters:
+	clust_count.append(clusters.count(unique_cluster))
+
+plt.figure()
+plt.hist(clust_count,bins=range(0,100,2),color='maroon',edgecolor='black')
+plt.title('Distribution of cluster sizes')
+plt.ylabel('No. of unique clusters')
+plt.xlabel('No. of cluster members')
+plt.xlim([0,60])
+plt.ylim([0,200])
+plt.grid()
+plt.show()
 
