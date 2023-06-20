b843ae820ca45eb257eea79b51ddb4fde9eefc2c
Darren Tan <ptan@alaska.edu>
2023-06-20 13:47:02 -0800

    Update timeline_checker to generate both seismic and infrasound

--- timeline_checker.py
+++ timeline_checker.py
@@ -5,25 +5,31 @@ from toolbox import check_timeline
 # Define variables for timeline checker function
 source = 'IRIS'
 network = 'AV'
-station = 'CERB,CESW,CEPE,CETU,CEAP,CERA'
+station = 'PN7A,PS1A,PS4A,PV6A,PVV'
 location = ''
-channel = '*HE'
+seis_channel = '*HZ'
+infra_channel = '*DF'
 
 # starttime = UTCDateTime(2021, 9, 14, 16, 0, 0)
 # starttime = UTCDateTime(2023, 3, 21, 13)
 # endtime = starttime + 3*3600
-starttime = UTCDateTime(2021, 7, 30, 8)
+starttime = UTCDateTime(2022, 7, 30, 10)
 endtime = starttime + 3*3600
-model_path = './models/4min_all_subsampled2_model.h5'
-meanvar_path = './models/4min_all_subsampled2_meanvar.npy'
+seis_model_path = './models/4min_all_subsampled2_model.h5'
+seis_meanvar_path = './models/4min_all_subsampled2_meanvar.npy'
+infra_model_path = './models/4min_all_infra_subsampled0_model.h5'
+infra_meanvar_path = './models/4min_all_infra_subsampled0_meanvar.npy'
 npy_dir = None
 spec_kwargs = None
 annotate = True
 export_path = None
 
 # Check timeline
-predmat = check_timeline(source, network, station, channel, location, starttime, endtime,
-                         model_path, meanvar_path, npy_dir=npy_dir, spec_kwargs=spec_kwargs,
+predmat = check_timeline(source, network, station, seis_channel, location, starttime, endtime,
+                         seis_model_path, seis_meanvar_path, npy_dir=npy_dir, spec_kwargs=spec_kwargs,
+                         annotate=annotate, export_path=export_path)
+predmat = check_timeline(source, network, station, infra_channel, location, starttime, endtime,
+                         infra_model_path, infra_meanvar_path, npy_dir=npy_dir, spec_kwargs=spec_kwargs,
                          annotate=annotate, export_path=export_path)
 
 # # Switch to infrasound
