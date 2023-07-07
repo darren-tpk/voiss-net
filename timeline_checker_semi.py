ac5751d70b5641843357cd8e0b179e2af2f452b9
Darren Tan <ptan@alaska.edu>
2023-07-07 12:46:33 -0800

    Update semi timeline checker with fig width and class cbar args

--- timeline_checker_semi.py
+++ timeline_checker_semi.py
@@ -7,36 +7,19 @@ source = 'IRIS'
 network = 'AV'
 station = 'CERB,CESW,CEPE,CETU,CEAP,CERA'
 location = ''
-channel = '*HE'
-# starttime = UTCDateTime(2021, 7, 30, 8)
-# endtime = starttime + 3*3600
-# starttime = UTCDateTime(2021, 8, 2, 3)
-# endtime = starttime + 3*3600
-# starttime = UTCDateTime(2021, 8, 11, 0)
-# endtime = starttime + 3*3600
-# starttime = UTCDateTime(2021, 6, 30, 11)
-# endtime = starttime + 3*3600
-starttime = UTCDateTime(2021, 7, 10, 19)
+seis_channel = '*HZ'
+starttime = UTCDateTime(2021, 7, 30, 8)
 endtime = starttime + 3*3600
-model_path = './models/4min_all_subsampled0_model_semi.h5'
-meanvar_path = './models/4min_all_subsampled0_meanvar_semi.npy'
+seis_model_path = './models/4min_all_subsampled0_model_semi.h5'
+seis_meanvar_path = './models/4min_all_subsampled0_meanvar_semi.npy'
 npy_dir = None
-spec_kwargs = {'freq_lims': (0.5,20)}
+fig_width = 34.5
+class_cbar = True
+spec_kwargs = {'freq_lims':(0.5,20)}
 annotate = True
 export_path = None
 
-# Check timeline
-predmat = check_timeline(source, network, station, channel, location, starttime, endtime,
-                         model_path, meanvar_path, npy_dir=npy_dir, spec_kwargs=spec_kwargs,
-                         annotate=annotate, export_path=export_path)
-
-# # Switch to infrasound
-# starttime = UTCDateTime(2021, 8, 6, 10, 0, 0)
-# endtime = starttime + 3*3600
-# channel = '*DF'
-# model_path = './models/4min_all_infra_subsampled0_model.h5'
-# meanvar_path = './models/4min_all_infra_subsampled0_meanvar.npy'
-#
-# # Check timeline
-# predmat = check_timeline(source, network, station, channel, location, starttime, endtime,
-#                          model_path, meanvar_path, npy_dir=npy_dir, annotate=annotate, export_path=export_path)
+# Check timeline for seismic
+predmat = check_timeline(source, network, station, seis_channel, location, starttime, endtime,
+                         seis_model_path, seis_meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
+                         spec_kwargs=spec_kwargs, class_cbar=class_cbar, annotate=annotate, export_path=export_path)
