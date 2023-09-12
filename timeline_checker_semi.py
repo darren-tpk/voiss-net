5f0a55ac3669c974379d7b3c78f208a6870e8c13
Darren Tan <ptan@alaska.edu>
2023-09-12 11:21:29 -0800

    Update timeline_checker_semi.py

--- timeline_checker_semi.py
+++ timeline_checker_semi.py
@@ -1,25 +1,25 @@
 # Import dependencies
 from obspy import UTCDateTime
-from toolbox import check_timeline
+from toolbox import check_timeline, check_timeline2
 
 # Define variables for timeline checker function
 source = 'IRIS'
 network = 'AV'
 station = 'CERB,CESW,CEPE,CETU,CEAP,CERA'
 location = ''
-seis_channel = '*HZ'
+channel = '*HE'
 starttime = UTCDateTime(2021, 7, 30, 8)
-endtime = starttime + 3*3600
-seis_model_path = './models/4min_all_subsampled0_model_semi.h5'
-seis_meanvar_path = './models/4min_all_subsampled0_meanvar_semi.npy'
+endtime = starttime + 6*3600
+model_path = './models/4min_all_augmented_semi_model.h5'
+meanvar_path = './models/4min_all_augmented_semi_meanvar.npy'
 npy_dir = None
-fig_width = 34.5
+fig_width = 38
 class_cbar = True
-spec_kwargs = {'freq_lims':(0.5,20)}
+spec_kwargs = {'freq_lims': (0.5,20)}
 annotate = True
-export_path = None
+export_path = '/Users/darrentpk/Desktop/'
 
 # Check timeline for seismic
-predmat = check_timeline(source, network, station, seis_channel, location, starttime, endtime,
-                         seis_model_path, seis_meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
-                         spec_kwargs=spec_kwargs, class_cbar=class_cbar, annotate=annotate, export_path=export_path)
+predmat = check_timeline2(source, network, station, channel, location, starttime, endtime,
+                         model_path, meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
+                         spec_kwargs=spec_kwargs, class_cbar=class_cbar, export_path=export_path)
