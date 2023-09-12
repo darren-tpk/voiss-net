ca519964242beba6e4ee0ca04770c14c10ef5162
Darren Tan <ptan@alaska.edu>
2023-09-12 11:32:24 -0800

    Add separate timeline_checker script for Shishaldin

--- npy_generator_scripts/create_unlabeled_dataset_infra.py
+++ npy_generator_scripts/create_unlabeled_dataset_infra.py
@@ -9,7 +9,7 @@ from waveform_collection import gather_waveforms
 from toolbox import process_waveform, calculate_spectrogram
 
 # Define filepaths and variables for functions
-starttime = UTCDateTime(2023, 1, 1, 00, 00)  # start time for data pull and spectrogram plot
+starttime = UTCDateTime(2021, 1, 1, 00, 00)  # start time for data pull and spectrogram plot
 endtime = UTCDateTime(2023, 3, 1, 00, 00)  # end time for data pull and spectrogram plot
 interval = 4 * 60  # Create a training dataset with 2D matrices spanning 4 minutes each
 time_step = 60  # Take steps of 1 minute
@@ -99,7 +99,7 @@ for i in range(num_days):
 
             # Skip matrices that have a spectrogram data gap
             if np.sum(spec_slice.flatten() < 0) > 50:
-                print('Skipping due to data gap, %d elements failed the check' % np.sum(spec_slice.flatten() < -220))
+                print('Skipping due to data gap, %d elements failed the check' % np.sum(spec_slice.flatten() < 0))
                 continue
 
             # Craft filename and save
---
+++ timeline_checker_shishaldin.py
@@ -0,0 +1,36 @@
+# Import dependencies
+from obspy import UTCDateTime
+from toolbox import check_timeline, check_timeline2
+
+# Define variables for timeline checker function
+source = 'IRIS'
+network = 'AV'
+station = 'SSBA,SSLN,SSLS'
+location = ''
+seis_channel = '*HZ'
+infra_channel = 'BDF'
+starttime = UTCDateTime(2023, 9, 5)
+endtime = starttime + 24*3600
+seis_model_path = './models/4min_all_augmented_drop_model.h5'
+seis_meanvar_path = './models/4min_all_augmented_drop_meanvar.npy'
+infra_model_path = './models/4min_all_augmented_infra_model.h5'
+infra_meanvar_path = './models/4min_all_augmented_infra_meanvar.npy'
+npy_dir = None
+fig_width = 38
+class_cbar = True
+spec_kwargs = None
+annotate = False
+export_path = '/Users/darrentpk/Desktop/'
+transparent = None
+
+# # Check timeline for seismic
+predmat = check_timeline2(source, network, station, seis_channel, location, starttime, endtime,
+                         seis_model_path, seis_meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
+                         spec_kwargs=spec_kwargs, class_cbar=class_cbar,
+                         export_path=export_path, transparent=transparent)
+
+# Check timeline for infrasound
+predmat = check_timeline2(source, network, station, infra_channel, location, starttime, endtime,
+                         infra_model_path, infra_meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
+                         spec_kwargs=spec_kwargs, class_cbar=class_cbar,
+                         export_path=export_path, transparent=transparent)
