bfb0e97f057f748085adb0a59ee80fcb9db1deb4
Darren Tan <ptan@alaska.edu>
2023-09-20 15:07:23 -0800

    Add generate_fig flag

--- timeline_checker.py
+++ timeline_checker.py
@@ -16,23 +16,24 @@ seis_meanvar_path = './models/4min_all_augmented_drop_meanvar.npy'
 infra_model_path = './models/4min_all_augmented_infra_model.h5'
 infra_meanvar_path = './models/4min_all_augmented_infra_meanvar.npy'
 npy_dir = None
+generate_fig = True
 fig_width = 34.5
 class_cbar = True
 spec_kwargs = None
 annotate = False
-export_path = '/Users/darrentpk/Desktop/'
+export_path = None
 transparent = None
 
 # # Check timeline for seismic
 predmat = check_timeline2(source, network, station, seis_channel, location, starttime, endtime,
-                         seis_model_path, seis_meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
-                         spec_kwargs=spec_kwargs, class_cbar=class_cbar,
-                         export_path=export_path, transparent=transparent)
+                          seis_model_path, seis_meanvar_path, npy_dir=npy_dir, generate_fig=generate_fig,
+                          fig_width=fig_width, spec_kwargs=spec_kwargs, class_cbar=class_cbar,
+                          export_path=export_path, transparent=transparent)
 
 # Check timeline for infrasound
 starttime = UTCDateTime(2021, 8, 6, 10)
 endtime = starttime + 3*3600
 predmat = check_timeline2(source, network, station, infra_channel, location, starttime, endtime,
-                         infra_model_path, infra_meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
-                         spec_kwargs=spec_kwargs, class_cbar=class_cbar,
-                         export_path=export_path, transparent=transparent)
+                          infra_model_path, infra_meanvar_path, npy_dir=npy_dir, generate_fig=generate_fig,
+                          fig_width=fig_width, spec_kwargs=spec_kwargs, class_cbar=class_cbar,
+                          export_path=export_path, transparent=transparent)
--- timeline_checker_semi.py
+++ timeline_checker_semi.py
@@ -13,13 +13,15 @@ endtime = starttime + 6*3600
 model_path = './models/4min_all_augmented_semi_model.h5'
 meanvar_path = './models/4min_all_augmented_semi_meanvar.npy'
 npy_dir = None
+generate_fig = True
 fig_width = 38
 class_cbar = True
 spec_kwargs = {'freq_lims': (0.5,20)}
-annotate = True
+# annotate = True
 export_path = None
 
 # Check timeline for seismic
-predmat = check_timeline2(source, network, station, channel, location, starttime, endtime,
-                         model_path, meanvar_path, npy_dir=npy_dir, fig_width=fig_width,
-                         spec_kwargs=spec_kwargs, class_cbar=class_cbar, export_path=export_path)
+class_mat, prob_mat = check_timeline2(source, network, station, channel, location, starttime, endtime,
+                                      model_path, meanvar_path, npy_dir=npy_dir, generate_fig=generate_fig,
+                                      fig_width=fig_width, class_cbar=class_cbar, spec_kwargs=spec_kwargs,
+                                      export_path=export_path)
