4a6f0a5afe750cb3f278308b54168a90fc501d58
Darren Tan <ptan@alaska.edu>
2023-09-22 12:52:55 -0800

    Update timeline plotting scripts

--- timeline_plot_pavlof.py
+++ timeline_plot_pavlof.py
@@ -10,10 +10,10 @@ meanvar_path = './models/4min_all_augmented_drop_meanvar.npy'
 
 # Plot timeline for 2013
 start_month = UTCDateTime(2013,2,13)
-end_month = UTCDateTime(2013,10,3)
+end_month = UTCDateTime(2013,9,30)
 npy_dir = '/Users/darrentpk/Desktop/all_npys/pavlof_2013_npy/'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (start_month.strftime('%Y-%m-%d'),end_month.strftime('%Y-%m-%d'))
-export_path = '/Users/darrentpk/Desktop/pavlof2013x.png'
+export_path = '/Users/darrentpk/Desktop/Github/tremor_ml/figures/pavlof2013.png'
 plot_timeline(start_month, end_month, time_step, type, model_path, meanvar_path, npy_dir, plot_title,
               export_path=export_path, transparent=False)
 
@@ -22,7 +22,7 @@ start_month = UTCDateTime(2014,8,13)
 end_month = UTCDateTime(2015,2,26)
 npy_dir = '/Users/darrentpk/Desktop/all_npys/pavlof_2014_npy_2/'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (start_month.strftime('%Y-%m-%d'),end_month.strftime('%Y-%m-%d'))
-export_path = '/Users/darrentpk/Desktop/pavlof2014.png'
+export_path = '/Users/darrentpk/Desktop/Github/tremor_ml/figures/pavlof2014.png'
 plot_timeline(start_month, end_month, time_step, type, model_path, meanvar_path, npy_dir, plot_title,
               export_path=export_path, transparent=False)
 
@@ -31,7 +31,7 @@ start_month = UTCDateTime(2015,12,28)
 end_month = UTCDateTime(2016,11,4)
 npy_dir = '/Users/darrentpk/Desktop/all_npys/pavlof_2016_npy/'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (start_month.strftime('%Y-%m-%d'),end_month.strftime('%Y-%m-%d'))
-export_path = '/Users/darrentpk/Desktop/pavlof2016.png'
+export_path = '/Users/darrentpk/Desktop/Github/tremor_ml/figures/pavlof2016.png'
 plot_timeline(start_month, end_month, time_step, type, model_path, meanvar_path, npy_dir, plot_title,
               export_path=export_path, transparent=False)
 
@@ -41,7 +41,7 @@ end_month = UTCDateTime(2023,3,1)
 time_step = 60
 npy_dir = '/Users/darrentpk/Desktop/all_npys/pavlof_2021_2023_npy/'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (start_month.strftime('%Y-%m-%d'),end_month.strftime('%Y-%m-%d'))
-export_path = '/Users/darrentpk/Desktop/pavlof2021.png'
+export_path = '/Users/darrentpk/Desktop/Github/tremor_ml/figures/pavlof2021.png'
 plot_labels = True
 labels_kwargs = {'start_date': UTCDateTime(2021, 7, 22),
                  'end_date': UTCDateTime(2021, 9, 22),
