133908c448be070eb09c508bc2ad24f44bf8a45e
Darren Tan <ptan@alaska.edu>
2023-10-31 12:04:43 -0800

    Update timeline_checker with new models

--- timeline_checker.py
+++ timeline_checker.py
@@ -9,10 +9,10 @@ station = 'PN7A,PS1A,PS4A,PV6A,PVV'
 location = ''
 seis_channel = '*HZ'
 infra_channel = 'BDF'
-seis_model_path = './models/4min_all_augmented_new_model.h5'
-seis_meanvar_path = './models/4min_all_augmented_new_meanvar.npy'
-infra_model_path = './models/4min_all_augmented_infra_new_model.h5'
-infra_meanvar_path = './models/4min_all_augmented_infra_new_meanvar.npy'
+seis_model_path = './models/4min_all_augmented_revised_model.h5'
+seis_meanvar_path = './models/4min_all_augmented_revised_meanvar.npy'
+infra_model_path = './models/4min_all_augmented_infra_revised_model.h5'
+infra_meanvar_path = './models/4min_all_augmented_infra_revised_meanvar.npy'
 npy_dir = None
 generate_fig = True
 fig_width = 34.5
@@ -23,8 +23,8 @@ export_path = None
 transparent = None
 
 # # Check timeline for seismic
-starttime = UTCDateTime(2021, 9, 12, 20)
-endtime = starttime + 8*3600
+starttime = UTCDateTime(2021, 9, 14, 16)
+endtime = starttime + 3*3600
 class_mat, prob_mat = check_timeline2(source, network, station, seis_channel, location, starttime, endtime,
                                       seis_model_path, seis_meanvar_path, npy_dir=npy_dir, generate_fig=generate_fig,
                                       fig_width=fig_width, spec_kwargs=spec_kwargs, class_cbar=class_cbar,
