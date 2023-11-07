1b8ddd809973701620f78b2a426beab84ef1c68f
Darren Tan <ptan@alaska.edu>
2023-11-06 17:55:21 -0900

    Remove npy_dir argument

--- plotting_scripts/timeline_plot_pavlof.py
+++ plotting_scripts/timeline_plot_pavlof.py
@@ -11,7 +11,7 @@ station2 = 'PN7A,PS1A,PS4A,PV6A,PVV'
 location = ''
 model_path = './models/4min_all_augmented_revised_model.h5'
 meanvar_path = './models/4min_all_augmented_revised_meanvar.npy'
-overlap = 0.25
+overlap = 0.75
 spec_kwargs = None
 
 # Arguments for plot_timeline
@@ -22,12 +22,11 @@ channel = '*HZ'
 starttime = UTCDateTime(2007,8,1)
 endtime = UTCDateTime(2007,11,1)
 time_step = 60
-npy_dir = None
 indicators_path = './pavlof_2007_indicators.pkl'
 plot_path = './pavlof_2007.png'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (starttime.strftime('%Y-%m-%d'),endtime.strftime('%Y-%m-%d'))
 # generate_timeline_indicators(source, network, station, channel, location, starttime, endtime, model_path, meanvar_path,
-#                              overlap, npy_dir, spec_kwargs, export_path=indicators_path)
+#                              overlap, spec_kwargs, export_path=indicators_path)
 plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, plot_title, export_path=plot_path,
               transparent=False, plot_stations=station, plot_labels=False, labels_kwargs=None)
 
@@ -35,12 +34,11 @@ plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, 
 starttime = UTCDateTime(2013,2,13)
 endtime = UTCDateTime(2013,9,30)
 time_step = 60
-npy_dir = None
 indicators_path = './pavlof_2013_indicators.pkl'
 plot_path = './pavlof_2013.png'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (starttime.strftime('%Y-%m-%d'),endtime.strftime('%Y-%m-%d'))
 # generate_timeline_indicators(source, network, station1, channel, location, starttime, endtime, model_path, meanvar_path,
-#                              overlap, npy_dir, spec_kwargs, export_path=indicators_path)
+#                              overlap, spec_kwargs, export_path=indicators_path)
 plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, plot_title, export_path=plot_path,
               transparent=False, plot_stations=station1, plot_labels=False, labels_kwargs=None)
 
@@ -48,12 +46,11 @@ plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, 
 starttime = UTCDateTime(2014,8,13)
 endtime = UTCDateTime(2015,2,26)
 time_step = 60
-npy_dir = None
 indicators_path = './pavlof_2014_indicators.pkl'
 plot_path = './pavlof_2014.png'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (starttime.strftime('%Y-%m-%d'),endtime.strftime('%Y-%m-%d'))
 # generate_timeline_indicators(source, network, station1, channel, location, starttime, endtime, model_path, meanvar_path,
-#                              overlap, npy_dir, spec_kwargs, export_path=indicators_path)
+#                              overlap, spec_kwargs, export_path=indicators_path)
 plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, plot_title, export_path=plot_path,
               transparent=False, plot_stations=station1, plot_labels=False, labels_kwargs=None)
 
@@ -61,25 +58,23 @@ plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, 
 starttime = UTCDateTime(2015,12,28)
 endtime = UTCDateTime(2016,11,4)
 time_step = 60
-npy_dir = None
 indicators_path = './pavlof_2016_indicators.pkl'
 plot_path = './pavlof_2016.png'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (starttime.strftime('%Y-%m-%d'),endtime.strftime('%Y-%m-%d'))
 # generate_timeline_indicators(source, network, station1, channel, location, starttime, endtime, model_path, meanvar_path,
-#                              overlap, npy_dir, spec_kwargs, export_path=indicators_path)
+#                              overlap, spec_kwargs, export_path=indicators_path)
 plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, plot_title, export_path=plot_path,
               transparent=False, plot_stations=station1, plot_labels=False, labels_kwargs=None)
 
 # Plot timeline for 2021-2022 (seismic)
-starttime = UTCDateTime(2022,3,25)
+starttime = UTCDateTime(2021,1,1)
 endtime = UTCDateTime(2023,3,1)
 time_step = 60
-npy_dir = None
 indicators_path = './pavlof_2021_indicators.pkl'
 plot_path = './pavlof_2021.png'
 plot_title = 'Timeline for Pavlof Seismic Tremor (%s to %s)' % (starttime.strftime('%Y-%m-%d'),endtime.strftime('%Y-%m-%d'))
-generate_timeline_indicators(source, network, station2, channel, location, starttime, endtime, model_path, meanvar_path,
-                             overlap, npy_dir, spec_kwargs, export_path=indicators_path)
+# generate_timeline_indicators(source, network, station2, channel, location, starttime, endtime, model_path, meanvar_path,
+#                              overlap, spec_kwargs, export_path=indicators_path)
 plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, plot_title, export_path=plot_path,
               transparent=False, plot_stations=station2, plot_labels=False, labels_kwargs=None)
 
@@ -91,11 +86,10 @@ meanvar_path = './models/4min_all_augmented_infra_revised_meanvar.npy'
 starttime = UTCDateTime(2021,1,1)
 endtime = UTCDateTime(2023,3,1)
 time_step = 60
-npy_dir = None # '/home/ptan/tremor_ml/all_npys/pavlof_2021_2023_infra_npy/'
 indicators_path = './pavlof_2021_infra_indicators.pkl'
 plot_path = './pavlof_2021_infra.png'
 plot_title = 'Timeline for Pavlof Infrasound Tremor (%s to %s)' % (starttime.strftime('%Y-%m-%d'),endtime.strftime('%Y-%m-%d'))
-generate_timeline_indicators(source, network, station2, channel, location, starttime, endtime, model_path, meanvar_path,
-                             overlap, npy_dir, spec_kwargs, export_path=indicators_path)
+# generate_timeline_indicators(source, network, station2, channel, location, starttime, endtime, model_path, meanvar_path,
+#                              overlap, spec_kwargs, export_path=indicators_path)
 plot_timeline(starttime, endtime, time_step, type, model_path, indicators_path, plot_title, export_path=plot_path,
               transparent=False, plot_stations=station2, plot_labels=False, labels_kwargs=None)
