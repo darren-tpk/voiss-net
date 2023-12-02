c7996549f130ec6b61878c06d8f17510bbd3e8e7
Darren Tan <ptan@alaska.edu>
2023-12-01 15:45:34 -0900

    Revamp multidisciplinary plot to use new barcode plot instead of KDE

--- plotting_scripts/plot_multidisciplinary.py
+++ plotting_scripts/plot_multidisciplinary.py
@@ -1,14 +1,14 @@
 # Plot multidisciplinary plot, top to bottom order
 # 1. AVO color code
 # 2. PS1A Spectrogram (Seismic)
-# 2. Kernel Density Plot (Seismic)
 # 3. Barcode Plot (Seismic)
 # 4. Reduced displacement (Seismic)
-# 5. Kernel Density Plot (Infrasound)
-# 6. Barcode Plot (Infrasound)
-# 7. Spaced-based observations (Hotspot & SO2)
+# 5. Barcode plot (Infrasound)
+# 6. Spaced-based observations (Hotspot & SO2)
+# 7. Explosion time series
 
 # Import all dependencies
+import pickle
 import pandas as pd
 import numpy as np
 import colorcet as cc
@@ -19,40 +19,38 @@ from matplotlib.colors import ListedColormap
 from scipy.signal import medfilt
 
 # Plot settings
-common_norm=True
-multiple='fill'
+starttime = UTCDateTime(2021,1,1)
+endtime = UTCDateTime(2023,3,1)
+common_norm = True
+multiple = 'fill'
 bw_method = 1/10000
-outfile = '/Users/darrentpk/Desktop/multidisciplinary_fig_v4.png'
+outfile = '/Users/darrentpk/Desktop/multidisciplinary_fig_v6.png'
 
 # Define time ticks on x-axis
-base_time = UTCDateTime(2021,1,1)
-total_days = (UTCDateTime(2023,3,1)-base_time)/86400
 month_utcdatetimes = []
-for i in range(1,13):
-    month_utcdatetime = UTCDateTime(2021,i,1)
-    month_utcdatetimes.append(month_utcdatetime)
-for i in range(1,13):
-    month_utcdatetime = UTCDateTime(2022,i,1)
-    month_utcdatetimes.append(month_utcdatetime)
-for i in range(1,4):
-    month_utcdatetime = UTCDateTime(2023,i,1)
-    month_utcdatetimes.append(month_utcdatetime)
-xticks_horiz = [(t - base_time)/86400 for t in month_utcdatetimes]
+month_utcdate = starttime
+while month_utcdate <= endtime:
+    month_utcdatetimes.append(month_utcdate)
+    if month_utcdate.month + 1 <= 12:
+        month_utcdate += UTCDateTime(month_utcdate.year, month_utcdate.month + 1, 1) - month_utcdate
+    else:
+        month_utcdate += UTCDateTime(month_utcdate.year + 1, 1, 1) - month_utcdate
 xticklabels_horiz = [t.strftime('%b \'%y') for t in month_utcdatetimes]
-xticks_horiz = [t/total_days for t in xticks_horiz]
+total_days = (endtime-starttime)/86400
+xticks_horiz = [(t - starttime)/86400/total_days for t in month_utcdatetimes]
 
 # Define labeled start and end
 labeled_start = UTCDateTime(2021, 7, 22)
 labeled_end = UTCDateTime(2021, 9, 22)
-labeled_start_pct_since = (UTCDateTime(2021, 7, 22)-base_time)/86400/total_days
-labeled_end_pct_since = (UTCDateTime(2021, 9, 22)-base_time)/86400/total_days
+labeled_start_pct_since = (UTCDateTime(2021, 7, 22) - starttime)/86400/total_days
+labeled_end_pct_since = (UTCDateTime(2021, 9, 22) - starttime)/86400/total_days
 
 # Define AVO color code axvspan limits
-g2y_pct_since = (UTCDateTime(2021,7,9)-base_time)/86400/total_days
-y2o_pct_since = (UTCDateTime(2021,8,5)-base_time)/86400/total_days
-o2y_pct_since = (UTCDateTime(2022,12,17)-base_time)/86400/total_days
-y2g_pct_since = (UTCDateTime(2023,1,19)-base_time)/86400/total_days
-g2e_pct_since = (month_utcdatetimes[-1]-base_time)/86400/total_days
+g2y_pct_since = (UTCDateTime(2021,7,9)-starttime)/86400/total_days
+y2o_pct_since = (UTCDateTime(2021,8,5)-starttime)/86400/total_days
+o2y_pct_since = (UTCDateTime(2022,12,17)-starttime)/86400/total_days
+y2g_pct_since = (UTCDateTime(2023,1,19)-starttime)/86400/total_days
+g2e_pct_since = (month_utcdatetimes[-1]-starttime)/86400/total_days
 
 # Read spectrogram and reduced displacement data
 metrics_dir = './metrics/PS1A_BHZ_metrics/'
@@ -64,69 +62,53 @@ dr_nm = np.nanmedian(dr, axis=0)  # network median
 dr_mf = medfilt(dr_nm, kernel_size=7)  # median filter
 spec_db = np.load(metrics_dir + 'spec_db_all' + tag + '.npy')
 
-# Read seismic dataframe with all properties
-df = pd.read_csv ('./data_frames/seismic_df.csv', usecols=range(1,8))
-df['class'] = df['class'].replace(np.nan,'N/A')
-
-# Calculate days since 2021-01-01
-date_list = list(df['time'])
-pct_since_arr = np.array([(UTCDateTime(dt) - UTCDateTime(2021,1,1))/86400/total_days for dt in date_list])
-df['pct_since'] = pct_since_arr
-
-# Map classes into a continuous array for heatmap later
-class_order = ['Broadband Tremor',
-             'Harmonic Tremor',
-             'Monochromatic Tremor',
-             'Non-tremor Signal',
-             'Explosion',
-             'Noise',
-             'N/A']
-class_list = list(df['class'])
-class_index = np.array([np.uint8(class_order.index(c)) for c in class_list])
-
-# Define colors and hue order
+# Parse seismic dataframe
+daily_bin_df = pd.read_csv('./data_frames/daily_bin_df.csv', index_col=0)
+daily_bin_df['bb_alpha'] = np.sqrt(daily_bin_df['bb_count'] / 1440)
+daily_bin_df['ht_alpha'] = np.sqrt(daily_bin_df['ht_count'] / 1440)
+daily_bin_df['mt_alpha'] = np.sqrt(daily_bin_df['mt_count'] / 1440)
+daily_bin_df['nt_alpha'] = np.sqrt(daily_bin_df['nt_count'] / 1440)
+daily_bin_df['exp_alpha'] = np.sqrt(daily_bin_df['exp_count'] / 1440)
+daily_bin_df['noise_alpha'] = np.sqrt(daily_bin_df['noise_count'] / 1440)
+matrix_plot = np.ones((6, len(daily_bin_df)))
+for i in range(6):
+    matrix_plot[i, :] = matrix_plot[i, :] * i
+matrix_plot = np.flip(matrix_plot, axis=0)
+matrix_alpha = np.vstack((daily_bin_df['bb_alpha'], daily_bin_df['ht_alpha'], daily_bin_df['mt_alpha'], daily_bin_df['nt_alpha'], daily_bin_df['exp_alpha'], daily_bin_df['noise_alpha']))
+matrix_alpha = np.flip(matrix_alpha, axis=0)
 rgb_values = np.array([
-    [193,  39,  45],
-    [  0, 129, 118],
-    [  0,   0, 167],
-    [238, 204,  22],
-    [164,  98,   0],
-    [ 40,  40,  40],
-    [255, 255, 255]])
-rgb_ratios = rgb_values/255
-colors = {'Broadband Tremor': rgb_ratios[0],
-          'Harmonic Tremor': rgb_ratios[1],
-          'Monochromatic Tremor': rgb_ratios[2],
-          'Non-tremor Signal': rgb_ratios[3],
-          'Explosion': rgb_ratios[4],
-          'Noise': rgb_ratios[5],
-          'N/A': rgb_ratios[6]}
-hue_order = ['Broadband Tremor',
-             'Harmonic Tremor',
-             'Monochromatic Tremor',
-             'Non-tremor Signal',
-             'Explosion',
-             'Noise',
-             'N/A']
-listed_colors = np.concatenate((rgb_ratios, np.ones((np.shape(rgb_values)[0],1))), axis=1)
-listed_colors = ListedColormap(listed_colors)
-real_cbar_tick_interval = 2 * (7-1)/14
-real_cbar_ticks = np.arange(real_cbar_tick_interval/2,7-1,real_cbar_tick_interval)
+    [193, 39, 45],
+    [0, 129, 118],
+    [0, 0, 167],
+    [238, 204, 22],
+    [164, 98, 0],
+    [40, 40, 40]])
+rgb_keys = ['BT',
+            'HT',
+            'MT',
+            'EQ',
+            'EX',
+            'NO']
+rgb_ratios = rgb_values / 255
+cmap = ListedColormap(rgb_ratios)
+amap = np.ones([256, 4])
+amap[:, 3] = np.linspace(0, 1, 256)
+amap = ListedColormap(amap)
 
 # Load thermal data
 thermal_filepath = './data_frames/thermal_anomalies.csv'
 thermal = pd.read_csv(thermal_filepath)
 rp_time = list(thermal['image_time'])
-rp_pct_since = np.array([(UTCDateTime(t)-base_time)/86400/total_days for t in rp_time])
-nodata_pct_since = (UTCDateTime(2023,1,1)-base_time)/86400/total_days
-text_pct_since = (UTCDateTime(2023,2,1)-base_time)/86400/total_days
+rp_pct_since = np.array([(UTCDateTime(t)-starttime)/86400/total_days for t in rp_time])
+nodata_pct_since = (UTCDateTime(2023,1,1)-starttime)/86400/total_days
+text_pct_since = (UTCDateTime(2023,2,1)-starttime)/86400/total_days
 rp_values = np.array(list(thermal['Radiative Power (MW)']))
 
 # Load SO2 data
 so2_filepath = './data_frames/so2_em_rate.csv'
 so2 = pd.read_csv(so2_filepath)
 so2_date = list(so2['date'])
-so2_pct_since = np.array([((UTCDateTime(t)-base_time)/86400 + 0.5)/total_days  for t in so2_date])
+so2_pct_since = np.array([((UTCDateTime(t)-starttime)/86400 + 0.5)/total_days  for t in so2_date])
 so2_rates = np.array(list(so2['em_rate']))
 
 # Read infrasound spectrogram
@@ -135,62 +117,54 @@ tmpl_inf = np.load(metrics_dir_inf + 'tmpl_all' + tag + '.npy')
 tmpl_inf = tmpl_inf[0,:]  # only need 1 row for time
 spec_db_inf = np.load(metrics_dir_inf + 'spec_db_all' + tag + '.npy')
 
-# Read infrasound dataframe with all properties
-df_inf = pd.read_csv ('./data_frames/infrasound_df.csv', usecols=range(1,8))
-df_inf['class'] = df_inf['class'].replace(np.nan,'N/A')
-
-# Calculate days since 2021-01-01
-date_list_inf = list(df_inf['time'])
-pct_since_arr_inf = np.array([(UTCDateTime(dt) - UTCDateTime(2021,1,1))/86400/total_days for dt in date_list_inf])
-df_inf['pct_since'] = pct_since_arr_inf
-
-# Map classes into a continuous array for heatmap later
-class_order_inf = ['Infrasonic Tremor',
-                  'Explosion',
-                  'Wind Noise',
-                  'Electronic Noise',
-                  'N/A']
-class_list_inf = list(df_inf['class'])
-class_index_inf = np.array([np.uint8(class_order_inf.index(c)) for c in class_list_inf])
-
-# Define colors and hue order
+# Parse infra dataframe
+daily_bin_df_inf = pd.read_csv('./data_frames/daily_bin_df_infra.csv', index_col=0)
+daily_bin_df_inf['it_alpha'] = np.sqrt(daily_bin_df_inf['it_count'] / 1440)
+daily_bin_df_inf['exp_alpha'] = np.sqrt(daily_bin_df_inf['exp_count'] / 1440)
+daily_bin_df_inf['wn_alpha'] = np.sqrt(daily_bin_df_inf['wn_count'] / 1440)
+daily_bin_df_inf['en_alpha'] = np.sqrt(daily_bin_df_inf['en_count'] / 1440)
+matrix_plot_inf = np.ones((4, len(daily_bin_df_inf)))
+for i in range(4):
+    matrix_plot_inf[i, :] = matrix_plot_inf[i, :] * i
+matrix_plot_inf = np.flip(matrix_plot_inf, axis=0)
+matrix_alpha_inf = np.vstack((daily_bin_df_inf['it_alpha'], daily_bin_df_inf['exp_alpha'],
+                              daily_bin_df_inf['wn_alpha'], daily_bin_df_inf['en_alpha']))
+matrix_alpha_inf = np.flip(matrix_alpha_inf, axis=0)
 rgb_values_inf = np.array([
     [103,  52, 235],
     [235, 152,  52],
     [ 40,  40,  40],
-    [ 15,  37,  60],
-    [255, 255, 255]])
-rgb_ratios_inf = rgb_values_inf/255
-colors_inf = {'Infrasonic Tremor': rgb_ratios_inf[0],
-              'Explosion': rgb_ratios_inf[1],
-              'Wind Noise': rgb_ratios_inf[2],
-              'Electronic Noise': rgb_ratios_inf[3],
-              'N/A': rgb_ratios_inf[4]}
-hue_order_inf = ['Infrasonic Tremor',
-                 'Explosion',
-                 'Wind Noise',
-                 'Electronic Noise',
-                 'N/A']
-listed_colors_inf = np.concatenate((rgb_ratios_inf, np.ones((np.shape(rgb_values_inf)[0],1))), axis=1)
-listed_colors_inf = ListedColormap(listed_colors_inf)
-real_cbar_tick_interval_inf = 2 * (5-1)/10
-real_cbar_ticks_inf = np.arange(real_cbar_tick_interval_inf/2,5-1,real_cbar_tick_interval_inf)
+    [ 15,  37,  60]])
+rgb_keys_inf = ['IT',
+                'EX',
+                'WN',
+                'EN']
+rgb_ratios_inf = rgb_values_inf / 255
+cmap_inf = ListedColormap(rgb_ratios_inf)
 
-# Load explosion dataframe
-exp_df = pd.read_csv('./data_frames/explosion_df.csv', usecols=range(1,7))
-best_exp_df = exp_df[(exp_df['seismic_count']>=3) & (exp_df['infrasound_count']>=2)]
-best_exp_times = [UTCDateTime(t) for t in best_exp_df['time'].tolist()]
-best_exp_pct_since = [(t-base_time)/86400/total_days for t in best_exp_times]
+# # Craft explosion dataframe
+# exp_bool = np.logical_and(matrix_condensed==4, matrix_condensed_inf==1)
+# exp_times = [starttime + (i*60) for i in range(len(exp_bool)) if exp_bool[i]==True]
+# seis_exp_pnorm = matrix_pnorm[exp_bool==True]
+# infra_exp_pnorm = matrix_pnorm_inf[exp_bool==True]
+# df_exp = pd.DataFrame(list(zip(exp_times, seis_exp_pnorm, infra_exp_pnorm)), columns =['time', 'pnorm_seis','pnorm_infra'])
+# df_exp.to_csv('./data_frames/exp_df_pnorm.csv')
+
+# Parse explosion dataframes
+df_exp = pd.read_csv('./data_frames/exp_df_pnorm.csv', usecols=range(1,4))
+best_df_exp = df_exp[(df_exp['pnorm_seis']>=.6) & (df_exp['pnorm_infra']>=.6)]
+best_exp_times = [UTCDateTime(t) for t in best_df_exp['time'].tolist()]
+best_exp_pct_since = [(t-starttime)/86400/total_days for t in best_exp_times]
 rtm_df = pd.read_csv('./data_frames/pavlof_rtm.csv')
 rtm_dates = rtm_df['Date'].tolist()
 rtm_dists = rtm_df['Distance (M)'].tolist()
 rtm_amps = rtm_df['Stack Amplitude'].tolist()
 rtm_times = [UTCDateTime(rtm_dates[j]) for j in range(len(rtm_dates)) if ((rtm_dists[j] < 600) and rtm_amps[j]>0.9)]
-rtm_pct_since = [(t-base_time)/86400/total_days for t in rtm_times]
+rtm_pct_since = [(t-starttime)/86400/total_days for t in rtm_times]
 
 # Commence multidisciplinary plotting
 figsize = (37, 19)
-height_ratios=[1.5,7,9,7,7,7,9,2]
+height_ratios=[1.5,7,9,7,7,7,6,2]
 fig, axs = plt.subplots(8, 1, figsize=figsize, gridspec_kw={'height_ratios': height_ratios})
 
 # Plot AVO color code
@@ -220,21 +194,15 @@ axs[1].set_yticklabels(['0.5','5','10'],fontsize=18)
 axs[1].set_ylabel('PS1A.BHZ\nFrequency',fontsize=22)
 
 # Plot kernel plot
-sns.kdeplot(data=df[df['class']!='N/A'], x='pct_since', hue='class', hue_order=reversed(hue_order), fill=True,
-        palette=colors, linewidth=1, legend=False, ax=axs[2], common_norm=common_norm, multiple=multiple, bw_method=bw_method)
+axs[2].imshow(matrix_plot, cmap=cmap, interpolation='None', extent=[xticks_horiz[0], xticks_horiz[-1], 0, 6], aspect='auto', origin='lower')
+axs[2].imshow(1-matrix_alpha, cmap=amap, interpolation='None', extent=[xticks_horiz[0], xticks_horiz[-1], 0, 6], aspect='auto', origin='lower')
+axs[2].set_yticks(np.arange(0.5, 6.5, 1))
+axs[2].set_yticklabels(np.flip(rgb_keys))
 axs[2].set_xticks(xticks_horiz)
 axs[2].set_xticklabels([])
 axs[2].set(xlabel=None)
 axs[2].set_xlim([xticks_horiz[0], xticks_horiz[-1]])
-ax2_ylim = axs[2].get_ylim()
-new_ymin = ax2_ylim[1] * -0.2
-axs[2].imshow(class_index.reshape(1,len(class_index)), extent=[0, 1, new_ymin, 0], origin='lower', aspect='auto',
-              interpolation='None', cmap=listed_colors, alpha=0.75)
-axs[2].axhline(y=0, linewidth=2.5, color='k')
-axs[2].plot([labeled_start_pct_since, labeled_end_pct_since], [0.05*new_ymin, 0.05*new_ymin], 'k-', linewidth=5.5)
-axs[2].plot([labeled_start_pct_since, labeled_end_pct_since], [0.95*new_ymin, 0.95*new_ymin], 'k-', linewidth=5.5)
-axs[2].set_ylim([new_ymin, ax2_ylim[1]])
-axs[2].set_ylabel('Seismic\nClass Density',fontsize=22)
+axs[2].set_ylabel('Seismic\nClass',fontsize=22)
 axs[2].tick_params(axis='y', labelsize=18)
 
 # Plot reduced displacement
@@ -281,25 +249,19 @@ axs[5].set_yticklabels(['0.5','5','10'],fontsize=18)
 axs[5].set_ylabel('PS4A.BDF\nFrequency',fontsize=22)
 
 # Plot Infrasound ML classes
-sns.kdeplot(data=df_inf[df_inf['class']!='N/A'], x='pct_since', hue='class', hue_order=reversed(hue_order_inf), fill=True,
-        palette=colors_inf, linewidth=1, legend=False, ax=axs[6], common_norm=common_norm, multiple=multiple, bw_method=bw_method)
+axs[6].imshow(matrix_plot_inf, cmap=cmap_inf, interpolation='None', extent=[xticks_horiz[0], xticks_horiz[-1], 0, 4], aspect='auto', origin='lower')
+axs[6].imshow(1-matrix_alpha_inf, cmap=amap, interpolation='None', extent=[xticks_horiz[0], xticks_horiz[-1], 0, 4], aspect='auto', origin='lower')
+axs[6].set_yticks(np.arange(0.5, 4.5, 1))
+axs[6].set_yticklabels(np.flip(rgb_keys_inf))
 axs[6].set_xticks(xticks_horiz)
 axs[6].set_xticklabels([])
 axs[6].set(xlabel=None)
 axs[6].set_xlim([xticks_horiz[0], xticks_horiz[-1]])
-ax6_ylim = axs[6].get_ylim()
-new_ymin = ax6_ylim[1] * -0.2
-axs[6].imshow(class_index_inf.reshape(1,len(class_index_inf)), extent=[0, 1, new_ymin, 0], origin='lower', aspect='auto',
-              interpolation='None', cmap=listed_colors_inf, alpha=0.75)
-axs[6].axhline(y=0, linewidth=2.5, color='k')
-axs[6].plot([labeled_start_pct_since, labeled_end_pct_since], [0.05*new_ymin, 0.05*new_ymin], 'k-', linewidth=5.5)
-axs[6].plot([labeled_start_pct_since, labeled_end_pct_since], [0.95*new_ymin, 0.95*new_ymin], 'k-', linewidth=5.5)
-axs[6].set_ylim([new_ymin, ax6_ylim[1]])
-axs[6].set_ylabel('Infrasound\nClass Density',fontsize=22)
+axs[6].set_ylabel('Infrasound\nClass',fontsize=22)
 axs[6].tick_params(axis='y', labelsize=18)
 
 # Plot explosion times
-axs[7].text(0,0.95,'ML ($\geq$3 seismic,$\geq$2 infrasound)',fontsize=18,color='black',horizontalalignment='left',verticalalignment='top')
+axs[7].text(0,0.95,'ML ($\geq$0.6 $P_{norm}$ for both data types)',fontsize=18,color='black',horizontalalignment='left',verticalalignment='top')
 axs[7].text(0,-0.05,'RTM (stack amplitude $\geq$0.9)',fontsize=18,color='red',horizontalalignment='left',verticalalignment='bottom')
 axs[7].vlines(x=best_exp_pct_since, ymin=0.5, ymax=1, colors='black', ls='-', lw=0.5)
 axs[7].vlines(x=rtm_pct_since, ymin=0, ymax=0.5, colors='red', ls='-', lw=0.5)
@@ -311,5 +273,5 @@ axs[7].set_xticks(xticks_horiz)
 axs[7].set_xticklabels(xticklabels_horiz, fontsize=20, rotation=30)
 
 # Save figure and show on interactive
-fig.savefig(outfile, transparent=True)
+fig.savefig(outfile, transparent=False, bbox_inches='tight')
 fig.show()
