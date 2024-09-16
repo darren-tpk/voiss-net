4de32eca8daab47553ade413ebf3d7880f791418
Darren Tan <ptan@alaska.edu>
2024-09-16 13:32:29 -0800

    Update DataGenerator.py to accept running_x_mean=None and running_x_var=None

--- DataGenerator.py
+++ DataGenerator.py
@@ -50,20 +50,27 @@ class DataGenerator(keras.utils.Sequence):
             # store label
             y[i] = self.labels[id]
 
-        ## normalize the batch if it is training
-        #if self.is_training:
-        # 
-        #    x_mean = np.mean(x, axis=0)
-        #    x_var = np.var(x, axis=0)
-#
-        #    x = (x - x_mean) / np.sqrt(x_var + 1e-5)
-#
-#            self.running_x_mean = 0.9 * self.running_x_mean + 0.1 * x_mean
-#            self.running_x_var = 0.9 * self.running_x_var + 0.1 * x_var
-#        else:
-#            x = (x - self.running_x_mean) / np.sqrt(self.running_x_var + 1e-5)
+        # Standardize data if needed
+        # If training with running mean and variance, standardize and update running mean and variance
+        if self.is_training and self.running_x_mean is not None and self.running_x_var is not None:
 
-        # min-max scale by individual image
+            x_mean = np.mean(x, axis=0)
+            x_var = np.var(x, axis=0)
+
+            x = (x - x_mean) / np.sqrt(x_var + 1e-5)
+
+            self.running_x_mean = 0.9 * self.running_x_mean + 0.1 * x_mean
+            self.running_x_var = 0.9 * self.running_x_var + 0.1 * x_var
+
+        # If generating validation and test data generators, standardize with running mean and variance
+        elif not self.is_training and self.running_x_mean is not None and self.running_x_var is not None:
+            x = (x - self.running_x_mean) / np.sqrt(self.running_x_var + 1e-5)
+
+        # If not using running mean and variance, simply pass the data through
+        else:
+            pass
+
+        # Min-max scale by individual image
         for k in range(self.batch_size):
             x[k, :] = (x[k, :] - np.min(x[k, :])) / (np.max(x[k, :]) - np.min(x[k, :]))
 
