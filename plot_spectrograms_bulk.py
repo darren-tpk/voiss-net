660dee8d59b40133837e0d94b0bce15c33177b5a
Darren Tan <ptan@alaska.edu>
2024-12-18 11:22:13 -0900

    PEP8 clean up for plot_spectrograms_bulk

--- plot_spectrograms_bulk.py
+++ plot_spectrograms_bulk.py
@@ -6,29 +6,27 @@ from obspy import UTCDateTime, Stream
 import warnings
 
 # Define variables for functions
-source = 'IRIS'
-network = 'AV'
-station = 'PN7A,PS1A,PS4A,PV6A,PVV'
-location = ''
-channel = '*HZ'
-starttime = UTCDateTime(2021, 1, 1, 0, 0)  # start time for data pull and spectrogram plot
-endtime = UTCDateTime(2023, 3, 1, 0, 0)  # end time for data pull and spectrogram plot
-pad = 60  # padding length [s]
-local = False  # pull data from local
-data_dir = None  # local data directory if pulling data from local
-client = 'IRIS'  # FDSN client for data pull
-filter_band = None  # frequency band for bandpass filter
-window_duration = 10  # spectrogram window duration [s]
-freq_lims = (0.5,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
-log = False  # logarithmic scale in spectrogram
-demean = False  # remove the temporal mean of the plotted time span from the spectrogram matrix
-v_percent_lims = (20,97.5)  # colorbar limits
-export_path = './spectrograms/'
-export_spec = True  # export spectrogram with no axis labels
-verbose = False
+SOURCE = 'IRIS'
+NETWORK = 'AV'
+STATION = 'PN7A,PS1A,PS4A,PV6A,PVV'
+LOCATION = ''
+CHANNEL = '*HZ'
+STARTTIME = UTCDateTime(2021, 1, 1, 0, 0)  # start time for data pull and spectrogram plot
+ENDTIME = UTCDateTime(2023, 3, 1, 0, 0)  # end time for data pull and spectrogram plot
+PAD = 60  # padding length [s]
+CLIENT = 'IRIS'  # FDSN client for data pull
+FILTER_BAND = None  # frequency band for bandpass filter
+WINDOW_DURATION = 10  # spectrogram window duration [s]
+FREQ_LIMS = (0.5,10)  # frequency limits for output spectrogram. If `None`, the limits will be adaptive
+LOG = False  # logarithmic scale in spectrogram
+DEMEAN = False  # remove the temporal mean of the plotted time span from the spectrogram matrix
+V_PERCENT_LIMS = (20,97.5)  # colorbar limits
+EXPORT_PATH = './spectrograms/'
+EXPORT_SPEC = True  # export spectrogram with no axis labels
+VERBOSE = False
 
 # Dissect time steps and loop
-total_hours = int(np.floor((endtime-starttime)/3600))
+total_hours = int(np.floor((ENDTIME-STARTTIME)/3600))
 
 # Commence loop
 i = 0
@@ -38,21 +36,21 @@ while i < (total_hours):
     try:
 
         # Define time bounds for current iteration
-        t1 = starttime + i*3600
-        t2 = starttime + (i+1)*3600
+        t1 = STARTTIME + i*3600
+        t2 = STARTTIME + (i+1)*3600
         print('Now at %s...' % t1)
 
         # Turn off warnings if non-verbose option is selected
-        if not verbose:
+        if not VERBOSE:
             warnings.filterwarnings("ignore")
 
         # Load data using waveform_collection tool
-        stream = gather_waveforms(source=source, network=network, station=station, location=location, channel=channel, starttime=t1-pad, endtime=t2+pad, verbose=False)
+        stream = gather_waveforms(source=SOURCE, network=NETWORK, station=STATION, location=LOCATION, channel=CHANNEL, starttime=t1-PAD, endtime=t2+PAD, verbose=False)
 
         # Process waveform
-        stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=pad, taper_percentage=None, filter_band=filter_band, verbose=False)
+        stream = process_waveform(stream, remove_response=True, detrend=False, taper_length=PAD, taper_percentage=None, filter_band=FILTER_BAND, verbose=False)
         stream_default_order = [tr.stats.station for tr in stream]
-        desired_index_order = [stream_default_order.index(stn) for stn in station.split(',') if stn in stream_default_order]
+        desired_index_order = [stream_default_order.index(stn) for stn in STATION.split(',') if stn in stream_default_order]
         stream = Stream([stream[i] for i in desired_index_order])
 
     except:
@@ -63,9 +61,9 @@ while i < (total_hours):
 
     # Plot all spectrograms on one figure if stream is not empty
     if len(stream) != 0:
-        plot_spectrogram_multi(stream, t1, t2, window_duration, freq_lims, log=log, demean=demean,
-                               v_percent_lims=v_percent_lims, earthquake_times=None, explosion_times=None,
-                               db_hist=True, export_path=export_path, export_spec=export_spec)
+        plot_spectrogram_multi(stream, t1, t2, WINDOW_DURATION, FREQ_LIMS, log=LOG, demean=DEMEAN,
+                               v_percent_lims=V_PERCENT_LIMS, earthquake_times=None, explosion_times=None,
+                               db_hist=True, export_path=EXPORT_PATH, export_spec=EXPORT_SPEC)
         print('Spectrogram plotted.')
     else:
         print('Stream object is empty, skipping.')
